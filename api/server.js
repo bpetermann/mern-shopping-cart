53614c6c7f02332b8d28306f932c83351038e2d1
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-07-03 23:27:14 +0200

    Setup basic express server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(201).json({ message: 'Welcome to the MERN Shopping Cart!' });
+});
+
+// Routes
+app.use('/api/shop', require('./routes/shopRoutes'));
+app.use('/api/user', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
